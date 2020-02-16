6dff6a5581c6bba850fd36b83a173639ce38a7b4
Xun6 <13617920171@163.com>
2020-02-16 14:07:54 +0800

    重新分类文件夹

--- example_1/main.cpp
+++ example_1/main.cpp
@@ -17,34 +17,34 @@
 
 /**例题：输入两个正整数 m 和 n ，求其最大公约数和最小公倍数*/
 
-//#include <stdio.h>
+#include <stdio.h>
+
+int main()
+{
+    int p,r,n,m,temp;
+    printf("请输入两个正整数 n,m:");
+    scanf("%d,%d",&n,&m);
+    if(n < m)
+    {
+        temp = n;
+        n = m;
+        m = temp;
+    }
+    p = n * m;
+    while(m!= 0)
+    {
+        r = n % m;
+        n = m;
+        m = r;
+    }
+    printf("最大公约数为：%d\n",n);
+    printf("最小公倍数为：%d\n",p/n);
+    return 0;
+}
 //
-//int main()
-//{
-//    int p,r,n,m,temp;
-//    printf("请输入两个正整数 n,m:");
-//    scanf("%d,%d",&n,&m);
-//    if(n < m)
-//    {
-//        temp = n;
-//        n = m;
-//        m = temp;
-//    }
-//    p = n * m;
-//    while(m!= 0)
-//    {
-//        r = n % m;
-//        n = m;
-//        m = r;
-//    }
-//    printf("最大公约数为：%d\n",n);
-//    printf("最小公倍数为：%d\n",p/n);
-//    return 0;
-//}
-
-
-
-
+//
+//
+//
 /**例题：输入一行字符，分别统计出其中英文字母、空格、数字、其他字符的个数*/
 //#include <stdio.h>
 //
@@ -80,30 +80,4 @@
 
 
 
-/**例题 5*/
-#include <stdio.h>
-#include <math.h>
 
-int main()
-{
-    int S=0;
-    int x=1,n,i,a,a1=0,a2 = 0;
-    printf("请输入位数‘a’和‘n’的值：\n");
-    scanf("%d,%d",&a,&n);
-    i = n;
-    while(x<=n)
-    {
-        if(i < 1)
-        {
-            break;
-        }
-        a2 = pow(10,n-i)* a + a1;
-        S = S + a2;
-        a1 = a2;
-        x++;
-        i--;
-    }
-    printf("输出最后一位数值：%d\n",a2);
-    printf("输出各数相加总和是：%d\n",S);
-    return 0;
-}
