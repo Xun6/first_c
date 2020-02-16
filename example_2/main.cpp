89ff78ab86727f5a520a9d310de58ec5b5419da6
Xun6 <13617920171@163.com>
2020-02-16 15:10:37 +0800

    提交练习题 6

--- example_2/main.cpp
+++ example_2/main.cpp
@@ -7,28 +7,55 @@
 //
 
 /** 例题 5*/
+//#include <stdio.h>
+//#include <math.h>
+//
+//int main()
+//{
+//    int S=0;
+//    int x=1,n,i,a,a1=0,a2 = 0;
+//    printf("请输入位数‘a’和‘n’的值：\n");
+//    scanf("%d,%d",&a,&n);
+//    i = n;
+//    while(x<=n)
+//    {
+//        if(i >= 1)
+//        {
+//            a2 = pow(10,n-i)* a + a1;
+//            S = S + a2;
+//            a1 = a2;
+//            x++;
+//            i--;
+//        }
+//    }
+//    printf("输出最后一位数值：%d\n",a2);
+//    printf("输出各数相加总和是：%d\n",S);
+//    return 0;
+//}
+
+
+
+
+/**练习题 6*/
 #include <stdio.h>
-#include <math.h>
 
 int main()
 {
-    int S=0;
-    int x=1,n,i,a,a1=0,a2 = 0;
-    printf("请输入位数‘a’和‘n’的值：\n");
-    scanf("%d,%d",&a,&n);
-    i = n;
-    while(x<=n)
+    int i =1;
+//    long int S =0,n1 =1;
+    double S =0,n1 =1;
+    printf("(1!+2!+3!+4!+....+20!)的值为:\n");
+    while(i <= 20)
     {
-        if(i >= 1)
-        {
-            a2 = pow(10,n-i)* a + a1;
-            S = S + a2;
-            a1 = a2;
-            x++;
-            i--;
-        }
+        n1 = i * n1;
+        S = S + n1;
+//        n1 = n2;
+        i++;
     }
-    printf("输出最后一位数值：%d\n",a2);
-    printf("输出各数相加总和是：%d\n",S);
-    return 0;
+//    printf("%ld\n",S);
+    printf("%e\n",S);
 }
+
+
+
+
