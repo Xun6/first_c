6f9d9a29c68d01b8750f0b6c1929e54ed76ab0f0
Xun6 <13617920171@163.com>
2020-02-22 17:01:23 +0800

    第五章练习题（2020.2.22）

--- example_2/main.cpp
+++ example_2/main.cpp
@@ -140,41 +140,128 @@
 
 
 /**练习题 9*/
+//#include <stdio.h>
+//
+//int main()
+//{
+//    int n,i,sum =0,j;
+//    for(n =2; n <1000; n++)  //确保在1000之内 （外循环）
+//    {
+//        for(i =1; i <=n-1; i++)    //确保在该数内 循环求因子（内循环）
+//        {
+//            if(n % i==0)    //判断 是否为因子
+//            {
+//                sum = sum +i;     //求该数所有因子 总和
+//            }
+//        }
+//        if(sum ==n)    //判断 因子数总和是否与该数 相等
+//        {
+//            printf("%d its factors are ",n);
+//            for(j =1; j <n; j++)    //循环输出  所有因子数
+//            {
+//                if(j ==1)        //判断当 j 等于1时，直接输出，且终止本次循环
+//                {
+//                    printf("%d",j);
+//                    continue;
+//                }
+//                if(n % j==0)
+//                {
+//                    printf("，%d",j);
+//                }
+//            }
+//            printf("\n");
+//        }
+//        sum = 0;  //每一次内循环结束后，因子求和 sum归零 重新计算
+//    }
+//    return 0;
+//}
+
+
+
+
+
+
+/**练习题 10*/
+//#include <stdio.h>
+//
+//int main()
+//{
+//    double a1 =2,a2 =1,i,a3,S =0;
+//    for(i =1; i <=20; i++)
+//    {
+//        S =S +a1/a2;
+//        a3 = a1;
+//        a1 = a1 +a2;
+//        a2 =a3;
+//    }
+//    printf("该数列前20项之和为：%f\n",S);
+//    return 0;
+//}
+
+
+
+
+/**练习题 11*/
+//#include <stdio.h>
+//
+//int main()
+//{
+//    int j;
+//    double S =0,h1 =100;
+//    for(j =1; j <=10; j++)
+//    {
+//        S = S +h1 +h1/2;
+//        h1 = h1/2;
+//
+//    }
+//    printf("第10次落地时，共经过：%f米\n",S-h1);
+//    printf("第10次反弹：%f 米\n",h1);
+//}
+
+
+
+
+
+/**练习题 12*/
+//#include <stdio.h>
+//
+//int main()
+//{
+//    int i;
+//    int n =1;     //第10天的桃子数（需要反推导）
+//    for(i =1; i <10; i++)
+//    {
+//        n = (n + 1) *2;
+//    }
+//    printf("第一天共摘 %d 个桃子\n",n);
+//    return 0;
+//}
+
+
+
+
+/**练习题 13*/
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
-    int n,i,sum =0,j;
-    for(n =2; n <1000; n++)  //确保在1000之内 （外循环）
+    float x0,x1,a;
+    printf("输入一个x的值： ");
+    scanf("%f",&a);
+    x0 =a;       //假设一个初始值 x0，再求出 x1,再循环迭代求出满足条件的根
+    x1 =(x0 +a/x0) /2;
+    while(fabs(x1 -x0) >=1e-5)
     {
-        for(i =1; i <=n-1; i++)    //确保在该数内 循环求因子（内循环）
-        {
-            if(n % i==0)    //判断 是否为因子
-            {
-                sum = sum +i;     //求该数所有因子 总和
-            }
-        }
-        if(sum ==n)    //判断 因子数总和是否与该数 相等
-        {
-            printf("%d its factors are ",n);
-            for(j =1; j <n; j++)    //循环输出  所有因子数
-            {
-                if(j ==1)
-                {
-                    printf("%d",j);
-                    continue;
-                }
-                if(n % j==0)
-                {
-                    printf("，%d",j);
-                }
-            }
-            printf("\n");
-        }
-        sum = 0;  //每一次内循环结束后，因子求和 sum归零 重新计算
+        x0 =x1;
+        x1 =(x0 +a/x0) /2;
     }
-    return 0;
+    printf("该输入值 %3.2f 的根是：%7.5f\n",a,x1);
 }
+
+
+
+
 
 
 
